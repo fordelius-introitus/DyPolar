1e0c035fad4486bc4b23c3ac1b1da786b1528d35
BERNARDO PINHO DE ALMEIDA <026.079806@universidadesenaicimatec.edu.br>
2026-09-08 11:56:37 -0300

    improved login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Landing } from './pages/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: Login},
+    {path: 'about-us', component: Landing}
+];
