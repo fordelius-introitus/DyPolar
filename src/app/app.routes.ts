357b7450960caf106355824548685f47329bcead
BERNARDO PINHO DE ALMEIDA <026.079806@universidadesenaicimatec.edu.br>
2026-09-09 11:54:32 -0300

    improved header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Landing } from './pages/landing/landing';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: Login},
-    {path: 'about-us', component: Landing}
+    {path: 'about-us', component: Landing},
+    {path: 'home', component: Home}
 ];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header-component/header-component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
